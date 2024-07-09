765cc480c94c13703ee9b54bb2b3728df6f2923a
KTrain5369 <69028025+KTrain5169@users.noreply.github.com>
2024-07-09 20:08:30 +1000

    add: info page
    here we go again

---
+++ pages/info.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    class="flex flex-col gap-2 bg-[color:var(--brand-colour)] p-4 rounded-2xl gap-8"
+  >
+    <ProjectTitleBar>
+      <span
+        class="text-[1.5rem] sm:text-[2rem] md:text-[3rem] lg:text-[4rem] internalFont flex flex-row flex-wrap gap-3"
+      >
+        <span class="font-bold">
+          THERE ARE BETTER WAYS TO PLAY
+          <span class="brand-gradient">{{ minecraftText }}</span
+          >.</span
+        >
+      </span>
+    </ProjectTitleBar>
+
+    <p class="text-2xl">
+      Don't be forced to use Essential. There are countless open-source
+      alternatives to help you.
+    </p>
+
+    <NuxtLink class="w-fit" to="/alternatives">
+      <BigButton> <LucideArrowUpRight /> ALTERNATIVES </BigButton>
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ProjectTitleBar from "../components/projectTitleBar.vue"
+import BigButton from "../components/bigButton.vue"
+
+const minecraftText = ref("MINECRAFT")
+
+if (Math.floor(Math.random() * 10000) === 0) {
+  minecraftText.value = "MINCERAFT"
+}
+
+useSeoMeta({
+  title: "Home",
+  description:
+    "Essential has many issues. This website tells why, and provides alternatives.",
+  ogImage: "/img/icon512.png",
+})
+</script>
